7ab3f04ca3a70a02758e20bab9071938fd55d136
Phuc La <phuc.la@asnet.com.vn>
2019-11-06 16:54:54 +0700

    Create alterations const

---
+++ src/constants/alterations.js
@@ -0,0 +1,83 @@
+export default [
+  {
+    id: '1',
+    name: 'New Julian Two Silk-Jersey Wrap Dress',
+    imageUrl: 'http://s7d2.scene7.com/is/image/DVF/13150DVFNNAVY_A1?$pdp-main$',
+    alterations: [
+      {
+        id: '1',
+        label: 'Shorten Sleeves',
+        isChecked: false,
+        price: 10,
+      },
+      {
+        id: '2',
+        label: 'Taper Waist',
+        isChecked: false,
+        price: 60,
+      },
+      {
+        id: '3',
+        label: 'Shorten Length',
+        isChecked: false,
+        price: 0,
+      },
+    ],
+  },
+  {
+    id: '2',
+    name: 'Ciara Satin Crepe Pants',
+    imageUrl: 'http://s7d2.scene7.com/is/image/DVF/13150DVFNNAVY_A1?$pdp-main$',
+    alterations: [
+      {
+        id: '1',
+        label: 'Taper Waist',
+        isChecked: false,
+        price: 40,
+      },
+      {
+        id: '2',
+        label: 'Taper Legs',
+        isChecked: true,
+        price: 30,
+      },
+      {
+        id: '3',
+        label: 'Shorten Length',
+        isChecked: false,
+        price: 0,
+      },
+    ],
+  },
+  {
+    id: '3',
+    name: 'Victoria Blazer',
+    imageUrl: 'http://s7d2.scene7.com/is/image/DVF/13150DVFNNAVY_A1?$pdp-main$',
+    alterations: [
+      {
+        id: '1',
+        label: 'Alter Shoulders/Arms',
+        isChecked: true,
+        price: 120,
+      },
+      {
+        id: '2',
+        label: 'Shorten Sleeves',
+        isChecked: false,
+        price: 30,
+      },
+      {
+        id: '3',
+        label: 'Taper Waist w/ Lining',
+        isChecked: true,
+        price: 60,
+      },
+      {
+        id: '3',
+        label: 'Shorten Body Length',
+        isChecked: false,
+        price: 50,
+      },
+    ],
+  },
+]
