6af0fc9fd9b99cd3d11575dbe89f7098668695f6
Phuc La <phuc.la@asnet.com.vn>
2019-11-11 16:46:02 +0700

    Create unit test for booking item

---
+++ src/__tests__/components/BookingItem.test.js
@@ -0,0 +1,39 @@
+// Libs
+import React from 'react'
+import { shallow } from 'enzyme'
+import { shallowToJson } from 'enzyme-to-json'
+
+// Component
+import BookingItem from '../../components/BookingItem'
+
+const mockProps = {
+  title: "I would like to fit myself using Hemster's Fitting Kit",
+  type: 'fit-stylist',
+  handleSelectOption: jest.fn(),
+}
+
+const wrapper = (props = {}) => {
+  const component = shallow(<BookingItem {...props} />)
+
+  return component
+}
+
+describe('BookingItem component', () => {
+  // Snapshot
+  test('should render correctly BookingItem component default props', () => {
+    const component = wrapper()
+    expect(shallowToJson(component)).toMatchSnapshot()
+    expect(BookingItem.defaultProps.title).toEqual('')
+    expect(BookingItem.defaultProps.type).toEqual('fitting-kit')
+    expect(BookingItem.defaultProps.handleSelectOption()).toBeUndefined()
+  })
+
+  // Testing props
+  test('should be rendered with props', () => {
+    const wrapperWithProps = wrapper(mockProps)
+    expect(wrapperWithProps.find('.bookingItem__fit-stylist').length).toEqual(1)
+    expect(
+      wrapperWithProps.find('.bookingItem__title').props().children
+    ).toEqual(mockProps.title)
+  })
+})
