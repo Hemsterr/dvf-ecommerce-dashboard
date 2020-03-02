077406929cc71647c76debbc2b4424224e5dba62
Phuc La <phuc.la@asnet.com.vn>
2020-03-02 17:09:19 +0700

    Create responsive for fitting options screen

--- src/routers/fitting-options/FittingDetails.js
+++ src/routers/fitting-options/FittingDetails.js
@@ -21,12 +21,14 @@ class FittingDetails extends React.PureComponent<Props> {
     const options = fittingOptions.filter(item => item.type !== fittingOption)
 
     return (
-      <div className="fitting-options__fitting-details">
-        <BookingItem description={fitting.description} type={fittingOption} />
-        <FittingWorks
-          options={fittingSteps[fittingOption]}
-          title={fitting.title}
-        />
+      <div className="fitting-options__wrapper fitting-options__fitting-details">
+        <div className="fitting-options__selected">
+          <BookingItem description={fitting.description} type={fittingOption} />
+          <FittingWorks
+            options={fittingSteps[fittingOption]}
+            title={fitting.title}
+          />
+        </div>
         <div>
           {options.map(item => (
             <BookingItem
@@ -34,6 +36,7 @@ class FittingDetails extends React.PureComponent<Props> {
               type={item.type}
               key={item.type}
               isSmall
+              description={item.description}
             />
           ))}
         </div>
