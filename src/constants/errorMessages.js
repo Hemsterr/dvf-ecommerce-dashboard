1e19584d483b27505f7e4d916dd20c75f9ad8a7d
Phuc La <phuc.la@asnet.com.vn>
2020-03-05 16:10:20 +0700

    Update validation for landing page

--- src/constants/errorMessages.js
+++ src/constants/errorMessages.js
@@ -2,4 +2,5 @@ export default {
   REQUIRED_FIELD: 'This field is required.',
   EMAIL_FORMAT: 'Invalid email address',
   INVALID_ZIPCODE_FORMAT: 'Invalid zip code',
+  INVALID_ORDER_NUMBER: 'Invalid order number',
 }
